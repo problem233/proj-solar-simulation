bd6f8c78db37d363e4d6571936c9043af8789524
problem233 <problem233@outlook.com>
2019-03-16 01:57:08 +0800

    split out drawing function & add some constants

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,16 @@ const length = ([x, y]: [number, number]) => Math.sqrt(x * x + y * y)
 const multiSimulate = (state: State, T: number, steps: number): State =>
   steps > 0 ? multiSimulate(simulate(state, T), T, steps - 1) : state
 
+function drawState (ctx: CanvasRenderingContext2D, scale: number, state: State) {
+  ctx.fillStyle = "white"
+  ctx.fillStyle = "yellow"
+  ctx.fill(circle(ctx.canvas.width / 2, ctx.canvas.height / 2, 50))
+  ctx.fillStyle = "gray"
+  ctx.fill(circle(
+    ctx.canvas.width / 2 + state.m_pos[0] * scale,
+    ctx.canvas.height / 2 - state.m_pos[1] * scale, 10))
+}
+
 window.onload = () => {
   const view = <HTMLDivElement> document.getElementById('view')
 
@@ -34,26 +44,31 @@ window.onload = () => {
   canvas.width = view.clientWidth
   view.appendChild(canvas)
 
+  // simulation related constants
   const scale = canvas.height / 3.1e11
-  const viewSpeed = 3600 * 25 * 10 // 5 days / s
+  const viewSpeed = 3600 * 25 * 10 // 10 days / s
   const framerate = 50
   const simSpeed = framerate * 100 // 5000 steps / s
 
+  // calculated constants
   const T = viewSpeed / simSpeed
+  const steps = simSpeed / framerate
+  const frameTime = 1000 / framerate
+
+  // state store
+  let stateStore = data.mercury
+  let paused = false
 
   const ctx = <CanvasRenderingContext2D> canvas.getContext('2d')
 
+  drawState(ctx, scale, stateStore)
+
   function frame (state: State) {
+    stateStore = state
     clearCanvas(ctx)
-    ctx.fillStyle = "white"
-    ctx.fillText("v: " + length(state.m_v).toExponential(3), 0, 10)
-    ctx.fillText("r: " + length(state.m_pos).toExponential(3), 0, 20)
-    ctx.fillStyle = "yellow"
-    ctx.fill(circle(canvas.width / 2, canvas.height / 2, 50))
-    ctx.fillStyle = "gray"
-    ctx.fill(circle(canvas.width / 2 + state.m_pos[0] * scale,
-                    canvas.height / 2 - state.m_pos[1] * scale, 10))
-    setTimeout(() => frame(multiSimulate(state, T, simSpeed / framerate)), 1000 / framerate)
+    drawState(ctx, scale, state)
+    if (paused) paused = false
+    else setTimeout(() => frame(multiSimulate(state, T, steps)), frameTime)
   }
   frame(data.mercury)
 }
